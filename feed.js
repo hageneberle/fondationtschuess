7c93481901521a57dfb1ef69b81612530346f514
hageneberle <118692795+hageneberle@users.noreply.github.com>
2026-05-25 14:18:21 +0200

    Update feed.js

--- feed.js
+++ feed.js
@@ -52,7 +52,7 @@
       left: 50%;
       transform: translateX(-50%);
       width: 100vw;
-      height: 70vh;
+      height: 45vh;
       background: ${barColor};
     `;
     return bar;
@@ -65,7 +65,7 @@
     block.className = 'ft-block';
     block.dataset.slug = slug;
     block.style.cssText = `
-      min-height: 70vh;
+      min-height: 45vh;
       box-sizing: border-box;
       user-select: text;
       -webkit-user-select: text;
